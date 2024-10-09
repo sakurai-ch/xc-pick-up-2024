584c1933a08cedeb7599bb1a0a882c9be1db9524
sakurai <sakurai_ch@yahoo.co.jp>
2024-10-09 09:27:22 +0900

    set: 2024 class5

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,8 @@ function App() {
         sx={{ bgcolor: 'tan', color: 'black'}}
       >
         <Toolbar>
-          {/* TODO 大会名をAIPから取得できるようにする */}
-          <h2>2024 J1 日本選手権</h2>
+          {/* TODO 大会名をAPIから取得できるようにする */}
+          <h2>2024 Class5 日本選手権</h2>
         </Toolbar>
       </AppBar>
       <Box mt={12} mx={2}>
